ff9d4e455b8ea6a0163c7a0cdf93095dd879c072
quirinecker <quirin.ecker@bajupa.com>
2025-05-06 19:50:24 +0200

    removed options

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,13 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	target: 'static',
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui'],
 	app: {
 		baseURL: process.env.BASE_URL,
 	},
-	generate: {
-		fallback: true,
-	}
 })
