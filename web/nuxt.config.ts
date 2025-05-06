c6a41e2ff961bc65e7a3fc5c811390d8f817d8b7
quirinecker <quirin.ecker@bajupa.com>
2025-05-06 18:41:48 +0200

    added deployment for frontent github pages

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui']
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui'],
+	app: {
+		baseURL: process.env.BASE_URL || 'http://localhost:3000',
+	}
 })
